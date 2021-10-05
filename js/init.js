5a03cb748a0d05abb6ad041ceb9d84c61dc6ff62
xiakele <xiakele123@gmail.com>
2021-10-05 15:29:27 +0800

    [修复] 使js更易读

--- js/init.js
+++ js/init.js
@@ -1,22 +1,23 @@
 time = 0;
 ws = wsGet();
 ans = gen(ws);
+let usrAns = document.querySelector("#num");
+let stat = document.querySelector("#status");
 // console.log(ans);
 document.querySelector("#submit").addEventListener("click", submit);
-document.querySelector("#num").addEventListener("keyup", function () {
-    if (verify(this.value, ws)) {
-        document.querySelector("#status").innerHTML = "";
+usrAns.addEventListener("keyup", function () {
+    if (verify(usrAns.value, ws)) {
+        stat.innerHTML = "";
     } else {
-        document.querySelector("#status").innerHTML = "<font color=\"red\">输入错误！</font>";
+        stat.innerHTML = "<font color=\"red\">输入错误！</font>";
     }
 });
-document.querySelector("#num").addEventListener("keydown", (key) => {
+usrAns.addEventListener("keydown", (key) => {
     if (key.key === "Enter") {
         submit();
     }
 });
 function submit() {
-    let usrAns = document.querySelector("#num");
     if (verify(usrAns.value, ws)) {
         time++;
         if (compare(ans, usrAns) === "correct") {
